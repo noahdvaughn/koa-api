fa1121454f358df54c0d44cd6770b195e8a6f839
Noah Vaughn <noahdvaughn@gmail.com>
2024-06-26 19:40:16 -0400

    working route

--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 
 const app = new Koa()
-
+export const router = new Router()
 const port = 3000
+
+router.get('/cool', (ctx, next) => {
+  ctx.body = 'Koa is cool'
+})
+
+app.use(router.routes())
 
 app.listen(port, () => {
   console.log('We love Koa')
